a72fe5652bdf62a56af2145a88e4d339fab23bb1
jwdavis <jeff@jwdavis.me>
2017-05-26 11:04:22 -0700

    disclaimer

--- lms.py
+++ lms.py
@@ -1,3 +1,12 @@
+"""
+Copyright SuccessOps, LLC 2017
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from flask import Flask
 from flask import request, redirect, render_template, url_for
 from flask import Response
