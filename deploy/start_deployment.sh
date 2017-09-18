fcfc3a77aff24e1623d5376f99912b27b3ecea30
jwdavis <jeff@jwdavis.me>
2017-09-17 21:04:42 -0700

    api, fw_rules, and branch fixes
    - add enable deployment manager to start script
    - add checkout deployment manager branch to transcode startup script
    - fix firewall rule import

--- deploy/start_deployment.sh
+++ deploy/start_deployment.sh
@@ -3,6 +3,7 @@
 sed -i -e "s/<sql-pass>/$1/g" deploy-gce-demo.yaml
 sed -i -e "s/<sup-pass>/$2/g" deploy-gce-demo.yaml
 
+gcloud service-management enable deploymentmanager.googleapis.com
 gcloud deployment-manager deployments create test --config deploy-gce-demo.yaml
 
 . ./finish_deployment.sh $1
