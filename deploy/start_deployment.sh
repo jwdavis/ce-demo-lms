73ca2493b1ee19715311a89b26daed0ae6a67bc4
Jeff Davis <jeff@highfive.com>
2017-10-23 12:30:43 -0400

    change deployment name
    deployment name is las

--- deploy/start_deployment.sh
+++ deploy/start_deployment.sh
@@ -4,6 +4,6 @@ sed -i -e "s/<sql-pass>/$1/g" deploy-gce-demo.yaml
 sed -i -e "s/<sup-pass>/$2/g" deploy-gce-demo.yaml
 
 gcloud service-management enable deploymentmanager.googleapis.com
-gcloud deployment-manager deployments create test --config deploy-gce-demo.yaml
+gcloud deployment-manager deployments create lms --config deploy-gce-demo.yaml
 
 . ./finish_deployment.sh $1
