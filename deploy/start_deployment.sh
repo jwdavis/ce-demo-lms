808faed9d2d06930514c2d34bddde5c8155e327c
jwdavis <jeff@jwdavis.me>
2017-11-01 15:38:56 -0700

    move 2 apis to pre-deploy
    - move sqladmin to start script
    - move compute to start script

--- deploy/start_deployment.sh
+++ deploy/start_deployment.sh
@@ -1,9 +1,22 @@
  #!/bin/bash
 
+# update file to use provided passwords
 sed -i -e "s/<sql-pass>/$1/g" deploy-gce-demo.yaml
 sed -i -e "s/<sup-pass>/$2/g" deploy-gce-demo.yaml
 
+# do sqladmin outside deployment manager due to timing delays
+# that sometime cause deployments to fail
+gcloud service-management enable sqladmin.googleapis.com
+
+# do compute engine outside of deployment so you can delete
+# deployment without disabling compute engine api
+gcloud service-management enable compute.googleapis.com
+
+# enable deployment manager api
 gcloud service-management enable deploymentmanager.googleapis.com
+
+# create the deployment
 gcloud deployment-manager deployments create lms --config deploy-gce-demo.yaml
 
+# finish up with post-deployment actions
 . ./finish_deployment.sh $1
