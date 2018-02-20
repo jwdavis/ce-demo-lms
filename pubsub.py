369af9c7a908c8e1c247b9b8b7aa54330421bcf7
Jeff Davis <jeff@highfive.com>
2018-02-20 04:42:22 +0100

    pubsub formatting
    pubsub formatting

--- pubsub.py
+++ pubsub.py
@@ -15,7 +15,7 @@
 from google.cloud import pubsub
 
 # publish message to topic
-def publish(topic,message,uri):
+def publish(topic, message, uri):
 	client = pubsub.Client()
 	topic = client.topic(topic)
 	return topic.publish(message,uri=uri)
@@ -24,4 +24,4 @@ def publish(topic,message,uri):
 def stuff_queue(topic):
 	client = pubsub.Client()
 	topic = client.topic(topic)
-	return topic.publish(b'stuffing queue',uri='q_stuffing')
+	return topic.publish(b'stuffing queue', uri='q_stuffing')
